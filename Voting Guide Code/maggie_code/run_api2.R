ac5cae5d63a38b3cf6e5397c7a96e889b989b656
Maggie Green <63611051+mgreen27605@users.noreply.github.com>
2024-10-09 16:08:45 -0500

    Update run_api2.R

--- Voting Guide Code/maggie_code/run_api2.R
+++ Voting Guide Code/maggie_code/run_api2.R
@@ -2,8 +2,9 @@
 library(plumber)
 
 # Source the API definition file
-source("voting_api.R")  # Adjust the path to where 'voting_api.R' is located
+#source("voting_api.R")  # Adjust the path to where 'voting_api.R' is located
 
 # Serve the API
+port <- as.numeric(Sys.getenv("PORT", "8000"))
 api <- plumb("voting_api.R")  # Adjust the path as needed
 api$run(host = "0.0.0.0", port = 8000)
